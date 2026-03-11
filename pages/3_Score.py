26969bbd28a111321b410509d0dfd1b200bba05f
Juliana008-mmg <julianagoubali@gmail.com>
2026-03-11 14:26:38 +0100

    Delete pages directory

--- pages/3_Score.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Score")
-
-if "score" in st.session_state:
-    st.write("Votre score :", st.session_state.score)
-    st.write("Questions :", st.session_state.total)
-else:
-    st.write("Aucun exercice fait pour le moment.")
